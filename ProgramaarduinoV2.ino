b8f926d638ed38bdc78f1e343d0704372e802a4a
luisrubio27 <80167330+luisrubio27@users.noreply.github.com>
2022-05-16 13:30:09 +0200

    Este seria el programa del Arduino nuevo
    Este seria el programa del Arduino nuevo pero con modificaciones interesantes como que se apague un led y se encienda el otro cuando se pulse el botón además tiene en cuenta la entrada de datos a través del trabajo de visual como un "BLANCO" o "NEGRO" en función del jugador que mueva las piezas.

---
+++ ProgramaarduinoV2.ino
@@ -0,0 +1,53 @@
+
+int PULSADOR=3;
+int LED1=2;
+int LED2=4;
+int estado1=LOW;
+int estado2=LOW;
+String mensaje_entrada;
+String mensaje_salida;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(PULSADOR,INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2,OUTPUT);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+}
+
+void loop()  {
+ 
+
+   if(Serial.available()>0)
+   {
+    mensaje_entrada=Serial.readStringUntil('\0');
+
+  
+   
+      while(digitalRead(PULSADOR)==LOW);
+     estado1 = digitalRead(LED1);
+      digitalWrite(LED1, !estado1);
+     estado2 = digitalRead(LED2);
+     digitalWrite(LED2, !estado2);
+     while(digitalRead(PULSADOR)==HIGH);
+   }
+ 
+  if(mensaje_entrada=="BLANCO")
+  {
+     digitalWrite(LED1,HIGH);
+     digitalWrite(LED2,LOW);  
+  }
+  
+    else if(mensaje_entrada=="NEGRO")
+  {
+     digitalWrite(LED1,LOW);
+     digitalWrite(LED2,HIGH);  
+  }
+
+
+    delay(500);
+  
+}
